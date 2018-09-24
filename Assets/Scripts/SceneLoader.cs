d49a003b65f5eb6dc1b9b155d40a26f8774b2ba6
Alex Leibowitz <firezdog@yahoo.com>
2018-09-23 21:17:22 -0500

    setup for project

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void LoadNextScene()
+	{
+		int totalScenes = SceneManager.sceneCountInBuildSettings;
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		int nextSceneIndex = (currentSceneIndex + 1) % totalScenes;
+		SceneManager.LoadScene(nextSceneIndex);
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+
+}
