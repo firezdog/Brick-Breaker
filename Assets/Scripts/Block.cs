b788b88f6e548196cab390705511c882c736d657
Alex Leibowitz <firezdog@yahoo.com>
2018-10-01 22:38:37 -0500

    make blocks disappear before they are destroyed

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,6 +7,7 @@ public class Block : MonoBehaviour {
 	private void OnCollisionEnter2D(Collision2D collission) {
 		if (gameObject.tag == "block") {
 			GetComponent<AudioSource>().Play();
+			GetComponent<SpriteRenderer>().enabled = false;
 		}
 		Invoke("Break", 0.1f);
 	}
