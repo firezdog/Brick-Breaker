4b1b9e120fb84e8d243d3df5a7068209036dc2c7
Alex Leibowitz <firezdog@yahoo.com>
2018-10-10 22:46:29 -0500

    added ability to change game speed

---
+++ Assets/Scripts/State/GameStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour {
+
+	[SerializeField, Range(0,2)] public float speed = 1f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Time.timeScale = speed;
+	}
+}
